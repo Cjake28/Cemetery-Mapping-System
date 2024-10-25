605e27ddd94c24e18ebaa1fd9204affb2229471e
jake supnet <supnetcjs@gmail.com>
2024-10-26 07:49:39 +0800

    change consolelog in my verifychekcauthtoken(3)

--- Backend/middleware/checkAUthVerifytoken.js
+++ Backend/middleware/checkAUthVerifytoken.js
@@ -1,29 +1,31 @@
 import jwt from "jsonwebtoken";
 
 export const checkAuth_VerifyToken = (req, res, next) => {
-	console.log("checkAuth_VerifyToken", req.headers['authorization']);
-	const token = req.cookies.HimlayanToken || req.headers['authorization'];
-	 console.log("checkAuth_VerifyToken", token);
-	 
-	if (!token){
-		console.log('checkAuth: No token');
-		return res.status(200).json({ success: false, message: "No token" });
-	}
-	
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("checkAuth_VerifyToken headers", req.headers['authorization']);
+    console.log("All cookies:", req.cookies);
+    
+    const token = req.cookies.HimlayanToken || req.headers['authorization']?.split(" ")[1];
+    console.log("Token to verify:", token);
 
-		if (!decoded){
-			console.log('checkAuth: invalid Token');
-			return res.status(200).json({ success: false, message: "Invalid token" });
-		}
+    if (!token) {
+        console.log('checkAuth: No token');
+        return res.status(200).json({ success: false, message: "No token" });
+    }
+    
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-		req.userId = decoded.userId;
-		req.role = decoded.role;
-		req.name = decoded.name;
-		next();
-	} catch (error) {
-		console.log("Error in verifyToken ", error);
-		return res.status(500).json({ success: false, message: "Server error" });
-	}
+        if (!decoded) {
+            console.log('checkAuth: Invalid token');
+            return res.status(200).json({ success: false, message: "Invalid token" });
+        }
+
+        req.userId = decoded.userId;
+        req.role = decoded.role;
+        req.name = decoded.name;
+        next();
+    } catch (error) {
+        console.log("Error in verifyToken:", error);
+        return res.status(500).json({ success: false, message: "Server error" });
+    }
 };
