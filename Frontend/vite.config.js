82e4ff4b93d80fba18f77489cd6143c8bfb0f201
jake supnet <supnetcjs@gmail.com>
2024-10-25 14:39:29 +0800

    update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    proxy:{
-      '/api': 'https://cemetery-mapping-system.onrender.com',
-    }
+    proxy: {
+      '/api': {
+          target: 'https://cemetery-mapping-system.onrender.com',
+          changeOrigin: true,
+      }
+  }
   },
   plugins: [react()],
 })
