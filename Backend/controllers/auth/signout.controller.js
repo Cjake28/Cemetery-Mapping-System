f165c1ac5cbc8665fbc4a85093ee5be602836a23
jake supnet <supnetcjs@gmail.com>
2024-11-08 00:56:33 +0800

    change middleware(8)

--- Backend/controllers/auth/signout.controller.js
+++ Backend/controllers/auth/signout.controller.js
@@ -1,9 +1,10 @@
-export async function signout(req, res) {
-    res.cookie("HimlayanToken", "", { 
+
+export async function signout(req, res){
+	res.cookie("HimlayanToken", "", {
         httpOnly: true,
-        secure: ture, // Ensure it's only secure in production
-        sameSite: "None", // SameSite=None to allow cross-origin
-        expires: new Date(0) // Set expiration to a date in the past
+        secure:true,
+        sameSite: "None",
+        // domain: 'cemetery-mapping-system.onrender.com' 
     });
-    res.status(200).json({ success: true, message: "Signed out successfully" });
+	res.status(200).json({ success: true, message: "Signed out successfully" });
 };
