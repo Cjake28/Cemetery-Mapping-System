4962cbcc3614826ec31125d76776eb92bb516f9d
jake supnet <supnetcjs@gmail.com>
2024-11-07 23:16:36 +0800

    change middleware(1)

--- Backend/controllers/auth/signout.controller.js
+++ Backend/controllers/auth/signout.controller.js
@@ -1,4 +1,4 @@
 export async function signout(req, res){
-	res.clearCookie("HimlayanToken");
+	res.clearCookie("HimlayanToken", { path: '/' });
 	res.status(200).json({ success: true, message: "Signed out successfully" });
 };
