1ef5dc5d5afcbc2cf584bce3b885be599f6b789c
jake supnet <supnetcjs@gmail.com>
2024-11-14 02:10:58 +0800

    fix virtualtour

--- Frontend/src/pages/VirtualTour/virtualTour.jsx
+++ Frontend/src/pages/VirtualTour/virtualTour.jsx
@@ -4,36 +4,35 @@ import './VirtualTour.css'
 export default function VirtualTour() {
 
   const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const onFrameLoaded = (event) => {
-      if(event.data.cmd === 'frameloaded'){
-        const postOBJ = event.data?.data?.posts || [];
-        console.log('Event Data:', postOBJ);
-        const scnObj = postOBJ.filter((ScnID) => ScnID.title === '1');
-        const scnID = scnObj[0]?.id;
-        console.log(scnID);
-        window.KuulaPlayerAPI.load(event.data.uuid, scnID);
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const onFrameLoaded = (event) => {
+  //     if(event.data.cmd === 'frameloaded'){
+  //       const postOBJ = event.data?.data?.posts || [];
+  //       console.log('Event Data:', postOBJ);
+  //       const scnObj = postOBJ.filter((ScnID) => ScnID.title === '1');
+  //       const scnID = scnObj[0]?.id;
+  //       console.log(scnID);
+  //       window.KuulaPlayerAPI.load(event.data.uuid, scnID);
+  //       setLoading(false);
+  //     }
+  //   };
 
-    window.addEventListener('message', onFrameLoaded);
+  //   window.addEventListener('message', onFrameLoaded);
     
-    return () => {
-      // Cleanup event listeners
-      window.removeEventListener('message', onFrameLoaded);
-    };
-  }, []);
+  //   return () => {
+  //     // Cleanup event listeners
+  //     window.removeEventListener('message', onFrameLoaded);
+  //   };
+  // }, []);
 
   return (
     <div className="virtualTour">
-        {loading && <div style={{ width: '100%', height: '100%', display:'flex', justifyContent: 'center', alignItems: 'center'}}> <h1>Loading....</h1></div>}
+        {/* <div style={{ width: '100%', height: '100%', display:'flex', justifyContent: 'center', alignItems: 'center'}}> <h1>Loading....</h1></div> */}
         <iframe
-          style={{ width: '100%', height: '100%', display: loading ? 'none' : 'block'}}
+          style={{ width: '100%', height: '100%'}} //display: loading ? 'none' : 'block'
           src="https://kuula.co/share/collection/7ZX47?logo=1&info=1&fs=1&vr=0&sd=1&thumbs=3"
           allowFullScreen
         ></iframe>
-
     </div>
   );
 }
